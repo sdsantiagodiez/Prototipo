960ac292c3e20db3d5c369d692bfe634ac98e8d2
martinlj10 <martinlj10@hotmail.com>
2015-02-08 22:17:43 -0300

    DSDCUS_ falta ultimo bucle

--- Controlador/ControladorBusqueda.cs
+++ Controlador/ControladorBusqueda.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Datos;
+using Models;
 
 namespace Controlador
 {
@@ -10,29 +12,99 @@ namespace Controlador
     {
 
         public void buscarArticulos(string pCodDes)
-        { 
-            //Si el parametro es codigo original o descripcion
-                // Se llama al catalogo de articulos
-                // Se busca el articulo en el catalogo con el parametro
-                // Se crea un array de articulos encontrados
-                // se guardan los articulos que coincidan codigo o descripcion
-                // Se devuelve el array de articulos encontrados
-               
-            //Si el parametro es codigo articulo proveedor
-                // Se llama al catalogo de articulos proveedores
-                // se busca el articulo proveedor y se devuelve
+        {
+            string[][] respuesta = new string[5][];
 
-            //Si los articulos encontrados no es vacio
-                // Se hace un get codigo original y descripcion
-                //Se llama al catalogo articulo proveedores y se busca por codigo original
-                // Cuando se encuentra uno se lo agrega a articulos encontrados:ArtProveedor
+            CatalogoArticulos ca = new CatalogoArticulos();
 
-            //para cada Articulo encontrado
+            CatalogoArticuloProveedores cap = new CatalogoArticuloProveedores();
+
+            List<ModeloArticulos> aEncontrados = new List<ModeloArticulos>();
+
+            List<ModeloArticulos> aAll = new List<ModeloArticulos>();
+            aAll = ca.getAll();
+            
+            List<ModeloArticuloProveedores> apAll = new List<ModeloArticuloProveedores>();
+            apAll = cap.getAll();
+            ModeloArticuloProveedores ap = new ModeloArticuloProveedores();
+
+            int i = 0;
+
+       if(pCodDes.Length<=10)
+            {
+            
+            
+        
+            #region Busca en los Articulos si el parametro pasado coincide con alguno y lo agrega a aEncontrados
+            foreach (ModeloArticulos modArt in aAll) // el parametro es codigo original o descripcion
+            {
+                if (pCodDes.Equals(modArt.codigoOriginalArt) == true || pCodDes.Equals(modArt.codigoOriginalArt) == true)
+                {
+                    aEncontrados.Add(modArt);
+                }
+                else
+                {
+                    if (pCodDes.CompareTo(modArt.codigoOriginalArt) < 10 || pCodDes.CompareTo(modArt.codigoOriginalArt) < 100)
+                    {
+                        aEncontrados.Add(modArt);
+                    }
+                
+                }
+
+            }
+
+            #endregion
+            }
+            else
+            {
+            
+
+                foreach (ModeloArticuloProveedores modArtProv in apAll)
+                {
+                    if (modArtProv.codigoArProveedor.Equals(pCodDes) == true)
+                    {
+                        ap = modArtProv;
+                    }
+                }
+            }
+            
+
+            if (aEncontrados.Count != 0)
+            {
+                foreach (ModeloArticulos ae in aEncontrados)
+                {
+                    List<ModeloArticuloProveedores> AEncontrados = new List<ModeloArticuloProveedores>();
+
+                    foreach (ModeloArticuloProveedores apr in apAll)
+                    {
+                        if (apr.codigoOriginalArt.Equals(ae.codigoOriginalArt) == true)
+                        {
+                            AEncontrados.Add(apr);
+                        }
+                    }
+
+                    foreach (ModeloArticuloProveedores arp in AEncontrados)
+                    {
+                        respuesta[0][i] = arp.codigoOriginalArt;
+                        respuesta[0][i] = arp.descripArPro;
+                        respuesta[0][i] = arp.codigoArProveedor;
+                        respuesta[0][i] = arp.razonSocialProv;
+                        //respuesta[0][i] = falta valor venta. Agregar clase a models**
+                        i++;
+                    }
 
 
+                }
+            
+            }
+            else if (ap.codigoArProveedor != "")
+            { /*
+               Falta ultimo bucle DSD CUS 1.1
+               */
+            }
 
 
-
+        
         }
 
 
