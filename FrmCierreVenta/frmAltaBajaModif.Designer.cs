92145ba18cd05a229be2d434eac240942dbe564e
fferrero07 <ferrero.franco.07@gmail.com>
2015-02-11 19:23:29 -0300

    se agrego formulario altas bajas y modif

---
+++ FrmCierreVenta/frmAltaBajaModif.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Vista
+{
+    partial class frmAltaBajaModif
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAltaCliente = new System.Windows.Forms.Button();
+            this.btnModCliente = new System.Windows.Forms.Button();
+            this.btnElimCliente = new System.Windows.Forms.Button();
+            this.btnAltaUsuario = new System.Windows.Forms.Button();
+            this.pnlED = new System.Windows.Forms.Panel();
+            this.btnModUsuario = new System.Windows.Forms.Button();
+            this.btnElimUsuario = new System.Windows.Forms.Button();
+            this.btnElimProv = new System.Windows.Forms.Button();
+            this.btnModProv = new System.Windows.Forms.Button();
+            this.btnAltaProv = new System.Windows.Forms.Button();
+            this.btnElimArtic = new System.Windows.Forms.Button();
+            this.btnModArtic = new System.Windows.Forms.Button();
+            this.btnAltaArtic = new System.Windows.Forms.Button();
+            this.btnModArticProv = new System.Windows.Forms.Button();
+            this.btnElimArticProv = new System.Windows.Forms.Button();
+            this.btnAltaArticProv = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.pnlED.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAltaCliente
+            // 
+            this.btnAltaCliente.Location = new System.Drawing.Point(28, 39);
+            this.btnAltaCliente.Name = "btnAltaCliente";
+            this.btnAltaCliente.Size = new System.Drawing.Size(107, 23);
+            this.btnAltaCliente.TabIndex = 0;
+            this.btnAltaCliente.Text = "Nuevo Cliente";
+            this.btnAltaCliente.UseVisualStyleBackColor = true;
+            // 
+            // btnModCliente
+            // 
+            this.btnModCliente.Location = new System.Drawing.Point(28, 68);
+            this.btnModCliente.Name = "btnModCliente";
+            this.btnModCliente.Size = new System.Drawing.Size(107, 23);
+            this.btnModCliente.TabIndex = 1;
+            this.btnModCliente.Text = "Modificar Cliente";
+            this.btnModCliente.UseVisualStyleBackColor = true;
+            // 
+            // btnElimCliente
+            // 
+            this.btnElimCliente.Location = new System.Drawing.Point(0, 3);
+            this.btnElimCliente.Name = "btnElimCliente";
+            this.btnElimCliente.Size = new System.Drawing.Size(107, 23);
+            this.btnElimCliente.TabIndex = 2;
+            this.btnElimCliente.Text = "Eliminar Cliente";
+            this.btnElimCliente.UseVisualStyleBackColor = true;
+            // 
+            // btnAltaUsuario
+            // 
+            this.btnAltaUsuario.Location = new System.Drawing.Point(0, 52);
+            this.btnAltaUsuario.Name = "btnAltaUsuario";
+            this.btnAltaUsuario.Size = new System.Drawing.Size(107, 23);
+            this.btnAltaUsuario.TabIndex = 3;
+            this.btnAltaUsuario.Text = "Nuevo Usuario";
+            this.btnAltaUsuario.UseVisualStyleBackColor = true;
+            // 
+            // pnlED
+            // 
+            this.pnlED.Controls.Add(this.btnModArticProv);
+            this.pnlED.Controls.Add(this.btnElimArticProv);
+            this.pnlED.Controls.Add(this.btnAltaArticProv);
+            this.pnlED.Controls.Add(this.btnElimArtic);
+            this.pnlED.Controls.Add(this.btnModArtic);
+            this.pnlED.Controls.Add(this.btnAltaArtic);
+            this.pnlED.Controls.Add(this.btnModProv);
+            this.pnlED.Controls.Add(this.btnElimProv);
+            this.pnlED.Controls.Add(this.btnAltaProv);
+            this.pnlED.Controls.Add(this.btnElimUsuario);
+            this.pnlED.Controls.Add(this.btnModUsuario);
+            this.pnlED.Controls.Add(this.btnElimCliente);
+            this.pnlED.Controls.Add(this.btnAltaUsuario);
+            this.pnlED.Location = new System.Drawing.Point(28, 97);
+            this.pnlED.Name = "pnlED";
+            this.pnlED.Size = new System.Drawing.Size(603, 136);
+            this.pnlED.TabIndex = 4;
+            // 
+            // btnModUsuario
+            // 
+            this.btnModUsuario.Location = new System.Drawing.Point(0, 81);
+            this.btnModUsuario.Name = "btnModUsuario";
+            this.btnModUsuario.Size = new System.Drawing.Size(107, 23);
+            this.btnModUsuario.TabIndex = 5;
+            this.btnModUsuario.Text = "Modificar Usuario";
+            this.btnModUsuario.UseVisualStyleBackColor = true;
+            // 
+            // btnElimUsuario
+            // 
+            this.btnElimUsuario.Location = new System.Drawing.Point(0, 110);
+            this.btnElimUsuario.Name = "btnElimUsuario";
+            this.btnElimUsuario.Size = new System.Drawing.Size(107, 23);
+            this.btnElimUsuario.TabIndex = 6;
+            this.btnElimUsuario.Text = "Eliminar Usuario";
+            this.btnElimUsuario.UseVisualStyleBackColor = true;
+            // 
+            // btnElimProv
+            // 
+            this.btnElimProv.Location = new System.Drawing.Point(140, 110);
+            this.btnElimProv.Name = "btnElimProv";
+            this.btnElimProv.Size = new System.Drawing.Size(125, 23);
+            this.btnElimProv.TabIndex = 7;
+            this.btnElimProv.Text = "Eliminar Proveedor";
+            this.btnElimProv.UseVisualStyleBackColor = true;
+            // 
+            // btnModProv
+            // 
+            this.btnModProv.Location = new System.Drawing.Point(140, 81);
+            this.btnModProv.Name = "btnModProv";
+            this.btnModProv.Size = new System.Drawing.Size(125, 23);
+            this.btnModProv.TabIndex = 6;
+            this.btnModProv.Text = "Modificar Proveedor";
+            this.btnModProv.UseVisualStyleBackColor = true;
+            // 
+            // btnAltaProv
+            // 
+            this.btnAltaProv.Location = new System.Drawing.Point(140, 52);
+            this.btnAltaProv.Name = "btnAltaProv";
+            this.btnAltaProv.Size = new System.Drawing.Size(125, 23);
+            this.btnAltaProv.TabIndex = 5;
+            this.btnAltaProv.Text = "Nuevo Proveedor";
+            this.btnAltaProv.UseVisualStyleBackColor = true;
+            // 
+            // btnElimArtic
+            // 
+            this.btnElimArtic.Location = new System.Drawing.Point(295, 110);
+            this.btnElimArtic.Name = "btnElimArtic";
+            this.btnElimArtic.Size = new System.Drawing.Size(107, 23);
+            this.btnElimArtic.TabIndex = 10;
+            this.btnElimArtic.Text = "Eliminar Articulo";
+            this.btnElimArtic.UseVisualStyleBackColor = true;
+            // 
+            // btnModArtic
+            // 
+            this.btnModArtic.Location = new System.Drawing.Point(295, 81);
+            this.btnModArtic.Name = "btnModArtic";
+            this.btnModArtic.Size = new System.Drawing.Size(107, 23);
+            this.btnModArtic.TabIndex = 9;
+            this.btnModArtic.Text = "Modificar Articulo";
+            this.btnModArtic.UseVisualStyleBackColor = true;
+            // 
+            // btnAltaArtic
+            // 
+            this.btnAltaArtic.Location = new System.Drawing.Point(295, 52);
+            this.btnAltaArtic.Name = "btnAltaArtic";
+            this.btnAltaArtic.Size = new System.Drawing.Size(107, 23);
+            this.btnAltaArtic.TabIndex = 8;
+            this.btnAltaArtic.Text = "Nuevo Articulo";
+            this.btnAltaArtic.UseVisualStyleBackColor = true;
+            // 
+            // btnModArticProv
+            // 
+            this.btnModArticProv.Location = new System.Drawing.Point(431, 81);
+            this.btnModArticProv.Name = "btnModArticProv";
+            this.btnModArticProv.Size = new System.Drawing.Size(167, 23);
+            this.btnModArticProv.TabIndex = 12;
+            this.btnModArticProv.Text = "Modificar Articulo de Proveedor";
+            this.btnModArticProv.UseVisualStyleBackColor = true;
+            // 
+            // btnElimArticProv
+            // 
+            this.btnElimArticProv.Location = new System.Drawing.Point(431, 110);
+            this.btnElimArticProv.Name = "btnElimArticProv";
+            this.btnElimArticProv.Size = new System.Drawing.Size(167, 23);
+            this.btnElimArticProv.TabIndex = 13;
+            this.btnElimArticProv.Text = "Eliminar Articulo de Proveedor";
+            this.btnElimArticProv.UseVisualStyleBackColor = true;
+            // 
+            // btnAltaArticProv
+            // 
+            this.btnAltaArticProv.Location = new System.Drawing.Point(431, 52);
+            this.btnAltaArticProv.Name = "btnAltaArticProv";
+            this.btnAltaArticProv.Size = new System.Drawing.Size(167, 23);
+            this.btnAltaArticProv.TabIndex = 11;
+            this.btnAltaArticProv.Text = "Nuevo Articulo de Proveedor";
+            this.btnAltaArticProv.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(579, 260);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // frmAltaBajaModif
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 295);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.pnlED);
+            this.Controls.Add(this.btnModCliente);
+            this.Controls.Add(this.btnAltaCliente);
+            this.Name = "frmAltaBajaModif";
+            this.Text = "Altas, Bajas, y Modificaciones";
+            this.pnlED.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAltaCliente;
+        private System.Windows.Forms.Button btnModCliente;
+        private System.Windows.Forms.Button btnElimCliente;
+        private System.Windows.Forms.Button btnAltaUsuario;
+        private System.Windows.Forms.Panel pnlED;
+        private System.Windows.Forms.Button btnModArticProv;
+        private System.Windows.Forms.Button btnElimArticProv;
+        private System.Windows.Forms.Button btnAltaArticProv;
+        private System.Windows.Forms.Button btnElimArtic;
+        private System.Windows.Forms.Button btnModArtic;
+        private System.Windows.Forms.Button btnAltaArtic;
+        private System.Windows.Forms.Button btnModProv;
+        private System.Windows.Forms.Button btnElimProv;
+        private System.Windows.Forms.Button btnAltaProv;
+        private System.Windows.Forms.Button btnElimUsuario;
+        private System.Windows.Forms.Button btnModUsuario;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
