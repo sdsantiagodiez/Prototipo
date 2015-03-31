a1f88df49b4f4ad80e5ccffa47aacd16b7252607
martinlj10 <martinlj10@hotmail.com>
2015-03-31 19:05:09 -0300

    Se codifico parte del agregar articulo a Pedido. Falta definir Pedido y linea de pedido

--- Controladores/ControladorPedidoProveedor.cs
+++ Controladores/ControladorPedidoProveedor.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Datos;
+using Modelos;
 
 namespace Controlador
 {
-    public class ControladorPedidoProveedor
+    public class ControladorPedidoProveedor:Controlador
     {
+        public string agregarArticulo(string tipoCodigo, string codArtProv, int cantidad)
+        {
+            
+            CatalogoArticuloProveedores cap = new CatalogoArticuloProveedores();
+            ModeloArticuloProveedores ap = new ModeloArticuloProveedores();
+
+            ap = cap.buscarArticuloProveedor(tipoCodigo, codArtProv)[0];
+
+            /*Inicializar la linea de Pedido con El articulo de proveedor y la cantidad*/
+
+            /*Agregar la linea de Pedido al pedido Actual que lo deberia tener el mismo controlador*/
+
+
+            return "El se ha agregado un articulo al pedido";
+        }
     }
 }
