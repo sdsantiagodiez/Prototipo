e2a6c9d6c2d1c48b5c1bc8814add7a84808af17d
fferrero07 <ferrero.franco.07@gmail.com>
2015-10-13 20:48:15 -0300

    el formulario de cierre ahora imprime datos, no todos, no realiza calculos, y es tan feo que me da vergüenza subirlo, pero es una primera instancia

--- Vista/frmPedidoClienteCierre.cs
+++ Vista/frmPedidoClienteCierre.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,10 +37,12 @@ namespace Vista
             if (dialogResult == DialogResult.Yes)
             {
                 var ctrlVenta = new ControladorProcesarVenta();
+                
                 //descuento stock
                 //ctrlVenta.cerrarPedido(ventaActual);
-                //TODO -> IMPRESION
-                //ctrlVenta.print();
+
+                //imprimo recibo
+                this.print();
                 //seteo emitido para terminar transacción y cierro
                 emitido = true;
                 this.Close();
@@ -65,6 +68,138 @@ namespace Vista
             this.lblIvaNetoVar.Text = ivaNeto.ToString("0.##");
             Decimal total = Decimal.Parse(ctrlVenta.getTotal()) + ivaNeto;
             this.lblTotalVar.Text = total.ToString("0.##");
+        }
+
+        private void print()
+        {
+            PrintDialog pd = new PrintDialog();
+            PrintDocument pdoc = new PrintDocument();
+            PrinterSettings ps = new PrinterSettings();
+
+            Font font = new Font("Courier New", 15);
+            PaperSize psize = new PaperSize("Custom", 100, 200);
+
+            pd.Document = pdoc;
+            pd.Document.DefaultPageSettings.PaperSize = psize;
+
+            pdoc.DefaultPageSettings.PaperSize.Height = 820;
+            pdoc.DefaultPageSettings.PaperSize.Width = 520;
+            pdoc.PrintPage += new PrintPageEventHandler(pdoc_PrintPage);
+
+            DialogResult result = pd.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                PrintPreviewDialog pp = new PrintPreviewDialog();
+                pp.Document = pdoc;
+                result = pp.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    pdoc.Print();
+                }
+            }
+
+        }
+
+        void pdoc_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Font font = new Font("Courier New", 10);
+            float fontHeight = font.GetHeight();
+            int startX = 50;
+            int startY = 55;
+            int Offset = 40;
+
+            graphics.DrawString("Mundo Renault", new Font("Courier New", 14),
+                                new SolidBrush(Color.Black), startX, startY + Offset);
+
+            Offset = Offset + 20;
+            graphics.DrawString(this.cbxTipoComp.Text,
+                     new Font("Courier New", 14),
+                     new SolidBrush(Color.Black), startX, startY + Offset);
+
+            Offset = Offset + 20;
+            graphics.DrawString("No. Factura: " + this.txtNroComp.Text,
+                     new Font("Courier New", 14),
+                     new SolidBrush(Color.Black), startX, startY + Offset);
+            
+            Offset = Offset + 20;
+            graphics.DrawString("Forma de pago: " + this.cbxFormaPago.Text,
+                     new Font("Courier New", 14),
+                     new SolidBrush(Color.Black), startX, startY + Offset);
+
+            Offset = Offset + 20;
+            graphics.DrawString("Fecha: " + dtpFecha.Text,
+                     new Font("Courier New", 12),
+                     new SolidBrush(Color.Black), startX, startY + Offset);
+
+            Offset = Offset + 20;
+            graphics.DrawString("Descuento (%): " + txtDescuentoPorc.Text,
+                     new Font("Courier New", 12),
+                     new SolidBrush(Color.Black), startX, startY + Offset);
+
+            Offset = Offset + 20;
+            graphics.DrawString("Descuento ($): " + txtDescuentoNeto.Text,
+                     new Font("Courier New", 12),
+                     new SolidBrush(Color.Black), startX, startY + Offset);
+
+            Offset = Offset + 20;
+            graphics.DrawString("Seña ($): " + txtSenia.Text,
+                     new Font("Courier New", 12),
+                     new SolidBrush(Color.Black), startX, startY + Offset);
+
+            Offset = Offset + 20;
+            graphics.DrawString("IVA (%): " + txtIvaPorc.Text,
+                     new Font("Courier New", 12),
+                     new SolidBrush(Color.Black), startX, startY + Offset);
+
+            Offset = Offset + 20;
+            String underLine = "------------------------------------------";
+            graphics.DrawString(underLine, 
+                     new Font("Courier New", 10),
+                     new SolidBrush(Color.Black), startX, startY + Offset);
+
+
+            Offset = Offset + 20;
+            graphics.DrawString("Nombre: " + txtNombre.Text,
+                     new Font("Courier New", 12),
+                     new SolidBrush(Color.Black), startX, startY + Offset);
+
+            Offset = Offset + 20;
+            graphics.DrawString("Apellido: " + txtApellido.Text,
+                     new Font("Courier New", 12),
+                     new SolidBrush(Color.Black), startX, startY + Offset);
+
+            Offset = Offset + 20;
+            underLine = "------------------------------------------";
+            graphics.DrawString(underLine, 
+                     new Font("Courier New", 10),
+                     new SolidBrush(Color.Black), startX, startY + Offset);
+
+            foreach (ModeloLineaPedido linea in ventaActual)
+            {
+                Offset = Offset + 20;
+                graphics.DrawString("Articulo: " + linea.descripcion,
+                         new Font("Courier New", 12),
+                         new SolidBrush(Color.Black), startX, startY + Offset);
+
+                graphics.DrawString("Cantidad: " + linea.cantidadArticulos,
+                         new Font("Courier New", 12),
+                         new SolidBrush(Color.Black), startX + 300, startY + Offset);
+
+                graphics.DrawString("Total parcial: " + linea.valorParcial,
+                         new Font("Courier New", 12),
+                         new SolidBrush(Color.Black), startX + 500, startY + Offset);
+            }
+
+            Offset = Offset + 20;
+            graphics.DrawString("Total : " + ctrlVenta.getTotal(), 
+                     new Font("Courier New", 10),
+                     new SolidBrush(Color.Black), startX, startY + Offset);
+            
+            Offset = Offset + 20;
+            graphics.DrawString("Cajero: " + "Ronald McDonald", 
+                     new Font("Courier New", 10),
+                     new SolidBrush(Color.Black), startX, startY + Offset);
         }
     }
 }
