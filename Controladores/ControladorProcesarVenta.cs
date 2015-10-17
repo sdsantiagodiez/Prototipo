1d6cc09cddcbf038ac2f96cadeb47189e18530a9
sdsantiagodiez <sdsantiagodiez@gmail.com>
2015-10-17 10:32:06 -0300

    Se corrigen algunas cosas debido al merge

--- Controladores/ControladorProcesarVenta.cs
+++ Controladores/ControladorProcesarVenta.cs
@@ -10,22 +10,92 @@ namespace Controladores
 {
     public class ControladorProcesarVenta
     {
-       //funciones a programar
-        //crearPedido
+        //funciones a programar
         //buscarReserva
-        //getDetalles
-        //agregarArticulo
-        //modificarCantidadArticulo
-        //cerrarPedido
 
-        
+        private List<ModeloLineaPedido> artVentaActual;
+        public List<ModeloLineaPedido> getVentaActual()
+        {
+            return artVentaActual;
+        }
+
+
         //busqueda de articulos por codigo original o por descripcion
         public List<ModeloArticuloProveedores> buscarArticulos(string categBusq, string busqArt)
         {
             var ctlgArticuloProv = new CatalogoArticuloProveedores();
-            List<ModeloArticuloProveedores> articulos = ctlgArticuloProv.buscarArticuloProveedor(categBusq,busqArt);
-            return articulos;               
+            List<ModeloArticuloProveedores> articulos = ctlgArticuloProv.buscarArticuloProveedor(categBusq, busqArt);
+            return articulos;
         }
 
+        public void cerrarPedido(List<ModeloLineaPedido> ventaActual)
+        {
+            //descontar stock
+            throw new NotImplementedException();
+        }
+
+        public void print()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void crearPedido()
+        {
+            this.artVentaActual = new List<ModeloLineaPedido>();
+        }
+
+        public void borrarActual()
+        {
+            this.artVentaActual = new List<ModeloLineaPedido>();
+        }
+
+        public void removeFromVenta(ModeloLineaPedido linea)
+        {
+            this.artVentaActual.Remove(linea);
+        }
+
+        public string getTotal()
+        {
+            decimal total = 0;
+            foreach (ModeloLineaPedido linea in artVentaActual)
+            {
+                total = total + linea.valorParcial;
+            }
+            return total.ToString("0.##");
+        }
+
+        public bool exists(ModeloArticuloProveedores articulo)
+        {
+            //checkeo existencia de articulo en venta actual
+            bool flag = false;
+            foreach (ModeloLineaPedido linea in artVentaActual)
+            {
+                if (linea.codigoOriginalArt == articulo.codigoOriginal && linea.codigoArtProveedor == articulo.codigoArticuloProveedor)
+                {
+                    flag = true;
+                }
+            }
+            return flag;
+        }
+
+        public void addToVenta(ModeloArticuloProveedores articulo, Int32 cantidad)
+        {
+            //le cambio el formato  y lo agrego a la lista de articulos ya seleccionados
+            ModeloLineaPedido nuevaLinea = new ModeloLineaPedido(articulo, cantidad);
+            this.artVentaActual.Add(nuevaLinea);
+        }
+
+        public ModeloPersonas getCliente(string dni)
+        {
+            CatalogoPersonas catPer = new CatalogoPersonas();
+            //Ver si se puede cambiar a buscarlo por codigo entidad catPer.GetOne(codigoEntidad)
+            return catPer.getOnePorDNI(dni);
+        }
+
+        public void addClient(ModeloPersonas newCli)
+        {
+            CatalogoPersonas catPer = new CatalogoPersonas();
+            catPer.agregarNuevaEntidad(newCli);
+        }
     }
 }
