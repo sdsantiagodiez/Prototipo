f961b1736e1066355f2fe1eb3b90f1cc5d124ada
sdsantiagodiez <sdsantiagodiez@gmail.com>
2016-01-05 22:03:31 -0300

    Se agrega libreria de clases compartidas para incluir constantes, validaciones y demás que se usen en todas las capas del proyecto

---
+++ LibreriaClasesCompartidas/Constantes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaClasesCompartidas
+{
+    public static class Constantes
+    {
+        public static class TiposEntidad
+        {
+            public const string Persona = "PER"; 
+            public const string Proveedor = "PRO";
+ 
+            public static class TiposPersona
+            {
+                public const string Cliente = "CLI"; 
+                public const string Usuario = "USR";
+                public const string ContactoProveedor = "CPR";     
+            }
+            public static class TiposProveedor
+            {
+                //Se diferenciaría de TiposEntidad.Proveedor en que TiposEntidad.Proveedor es la clasificación que divide a las entidades entre proveedores y personas.
+                //Mientras que TiposProveedor.Proveedor es una subclase de TiposEntidad.Proveedor al nivel de Cliente, Usuario y ContactoProveedor
+                public const string Proveedor = "PRO";  
+            }
+        }
+    }
+}
---
+++ LibreriaClasesCompartidas/Validar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaClasesCompartidas
+{
+    public class Validar
+    {
+        public static bool validarValorNumerico(string pStringNumero)
+        {
+            int numero;
+            return Int32.TryParse(pStringNumero, out numero);
+        }
+    }
+}
