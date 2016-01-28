42a13623a143c3633912da109ba7e5a4dfcc5048
martinlj10 <martinlj10@hotmail.com>
2016-01-28 12:47:06 -0300

    Se agrego una nueva Clase de Reportes que contiene dos informes rdlc, un Formulario contenedor de reportes. Resta realizar asociaciones para los datos. No ejecutar por que no creo que funcione todavía.

---
+++ Reportes/Repo_FacturaA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reportes
+{
+    public partial class Repo_FacturaA : Form
+    {
+        public Repo_FacturaA()
+        {
+            InitializeComponent();
+        }
+
+        private void Repo_FacturaA_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
