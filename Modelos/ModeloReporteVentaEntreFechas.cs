eb4af6053c5ccc6ee62d88aeb9809c70cc4b81b9
martinlj10 <martinlj10@hotmail.com>
2016-02-07 14:25:20 -0300

    Se agrega el modelo para el reporte de ventas

---
+++ Modelos/ModeloReporteVentaEntreFechas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    class ModeloReporteVentaEntreFechas
+    {
+        public string codigo_cliente { get; set;}
+        public string nombre_cliente { get; set; }
+        public int cantidad_ventas { get; set; }
+        public int cantidad_articulos { get; set; }
+        public float monto_ventas { get; set; }
+
+
+    }
+}
