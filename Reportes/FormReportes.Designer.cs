1e12e722ab91f02329f7cdb8d77be9aae4f73358
martinlj10 <martinlj10@hotmail.com>
2016-02-09 22:02:39 -0300

    Se agregan archivos faltantes en el formulario de reportes para su generacion.

---
+++ Reportes/FormReportes.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Reportes
+{
+    partial class FormReportes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.ReporteBase = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.ModeloReportePedidoEntreFechasBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ModeloReportePedidoEntreFechasBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ReporteBase
+            // 
+            reportDataSource1.Name = "DSInformePedidos";
+            reportDataSource1.Value = this.ModeloReportePedidoEntreFechasBindingSource;
+            this.ReporteBase.LocalReport.DataSources.Add(reportDataSource1);
+            this.ReporteBase.LocalReport.ReportEmbeddedResource = "Reportes.PedidosEntreFechas.rdlc";
+            this.ReporteBase.Location = new System.Drawing.Point(12, 12);
+            this.ReporteBase.Name = "ReporteBase";
+            this.ReporteBase.Size = new System.Drawing.Size(803, 528);
+            this.ReporteBase.TabIndex = 0;
+            // 
+            // ModeloReportePedidoEntreFechasBindingSource
+            // 
+            this.ModeloReportePedidoEntreFechasBindingSource.DataSource = typeof(Modelos.ModeloReportePedidoEntreFechas);
+            // 
+            // FormReportes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 552);
+            this.Controls.Add(this.ReporteBase);
+            this.Name = "FormReportes";
+            this.Text = "FormReportes";
+            this.Load += new System.EventHandler(this.FormReportes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ModeloReportePedidoEntreFechasBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer ReporteBase;
+        private System.Windows.Forms.BindingSource ModeloReportePedidoEntreFechasBindingSource;
+    }
+}
---
+++ Reportes/FormReportes.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Modelos;
+using Microsoft.Reporting.WinForms;
+
+namespace Reportes
+{
+    public partial class FormReportes : Form
+    {
+        public string NombreEmpresa { get; set; }
+        public string DireccionEmpresa { get; set; }
+        public string TelefonoEmpresa { get; set; }
+        public string fechaVecimiendo { get; set; }
+
+        public FormReportes()
+        {
+            InitializeComponent();
+        }
+        
+        public FormReportes(ModeloReportePedidoEntreFechas p_pedidoentrefechas)
+        {   //este constructor se construye al modelo de pedido entre fechas                    
+            this.ReporteBase.LocalReport.ReportEmbeddedResource ="Reportes.PedidosEntreFechas.rdlc";
+            this.ReporteBase.LocalReport.Refresh();
+            this.ReporteBase.RefreshReport();
+            this.ReporteBase.LocalReport.DataSources.Clear();
+            this.ReporteBase.LocalReport.DataSources.Add(new ReportDataSource("DSInformePedidos", p_pedidoentrefechas));
+            this.ReporteBase.RefreshReport();
+        }
+
+        public FormReportes(ModeloReporteVentaEntreFechas p_ventaentrefechas)
+        {   //este constructor se construye al modelo de pedido entre fechas                    
+            this.ReporteBase.LocalReport.ReportEmbeddedResource = "Reportes.VentaEntreFechas.rdlc";
+            this.ReporteBase.LocalReport.Refresh();
+            this.ReporteBase.RefreshReport();
+            this.ReporteBase.LocalReport.DataSources.Clear();
+            this.ReporteBase.LocalReport.DataSources.Add(new ReportDataSource("DSInformeVentas", p_ventaentrefechas));
+            this.ReporteBase.RefreshReport();
+        }
+
+
+        private void FormReportes_Load(object sender, EventArgs e)
+        {
+            //ReporteBase.LocalReport.DataSources.Clear();
+            SeteoParametrosGeneralesReporte();
+
+        }
+        //Setea los parametros generales de reporte como ser el nombre de la empresa, direccion telefono. Se podria
+        //Tener un menu donde podamos modificar dichas cosas. A plantearlo
+        private void SeteoParametrosGeneralesReporte()
+        {
+            ReportParameter[] parameters = new ReportParameter[4];
+            parameters[0] = new ReportParameter("parametroEmpresa", NombreEmpresa);
+            parameters[1] = new ReportParameter("parametroTelEmpresa", TelefonoEmpresa);
+            parameters[2] = new ReportParameter("parametroDirEmpresa", DireccionEmpresa);
+            parameters[3] = new ReportParameter("parametroFecVto", fechaVecimiendo);
+            this.ReporteBase.LocalReport.SetParameters(parameters);
+        }
+
+
+    }
+}
