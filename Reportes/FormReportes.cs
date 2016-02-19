fc3f3017f75dc57cf6ca955de864a5f9f4df96ba
martinlj10 <martinlj10@hotmail.com>
2016-02-19 17:56:10 -0300

    Se agregan los detalles al Frm de los pedidos y ventas. Tambien se agrega para generar una nota de pedido.

--- Reportes/FormReportes.cs
+++ Reportes/FormReportes.cs
@@ -14,33 +14,50 @@ namespace Reportes
 {
     public partial class FormReportes : Form
     {
-        public string NombreEmpresa { get; set; }
-        public string DireccionEmpresa { get; set; }
-        public string TelefonoEmpresa { get; set; }
-        public string fechaVecimiendo { get; set; }
+        public string glb_var_NombreEmpresa { get; set; }
+        public string glb_var_DireccionEmpresa { get; set; }
+        public string glb_var_TelefonoEmpresa { get; set; }
+        public string glb_var_fechaVecimiendo { get; set; }
 
         public FormReportes()
         {
             InitializeComponent();
         }
         
-        public FormReportes(ModeloReportePedidoEntreFechas p_pedidoentrefechas)
+        public FormReportes(ModeloReporteEncabezado p_pedidoentrefechas)
         {   //este constructor se construye al modelo de pedido entre fechas                    
             this.ReporteBase.LocalReport.ReportEmbeddedResource ="Reportes.PedidosEntreFechas.rdlc";
             this.ReporteBase.LocalReport.Refresh();
             this.ReporteBase.RefreshReport();
             this.ReporteBase.LocalReport.DataSources.Clear();
-            this.ReporteBase.LocalReport.DataSources.Add(new ReportDataSource("DSInformePedidos", p_pedidoentrefechas));
+            this.ReporteBase.LocalReport.DataSources.Add(new ReportDataSource("DSInformePedidoEncabezado", p_pedidoentrefechas));
+            this.ReporteBase.LocalReport.DataSources.Add(new ReportDataSource("DSInformePedidos", p_pedidoentrefechas.detallePedido));
             this.ReporteBase.RefreshReport();
         }
 
-        public FormReportes(ModeloReporteVentaEntreFechas p_ventaentrefechas)
+        public FormReportes(ModeloReporteEncabezado p_ventaentrefechas)
         {   //este constructor se construye al modelo de pedido entre fechas                    
             this.ReporteBase.LocalReport.ReportEmbeddedResource = "Reportes.VentaEntreFechas.rdlc";
             this.ReporteBase.LocalReport.Refresh();
             this.ReporteBase.RefreshReport();
             this.ReporteBase.LocalReport.DataSources.Clear();
-            this.ReporteBase.LocalReport.DataSources.Add(new ReportDataSource("DSInformeVentas", p_ventaentrefechas));
+            this.ReporteBase.LocalReport.DataSources.Add(new ReportDataSource("DSInformeVentaEncabezado", p_ventaentrefechas));
+            this.ReporteBase.LocalReport.DataSources.Add(new ReportDataSource("DSInformeVentas", p_ventaentrefechas.detalleVenta));
+            this.ReporteBase.RefreshReport();
+        }
+        public FormReportes(ModeloPedido p_ModeloPedido, ModeloPersonas p_ModeloPersonas)
+        {   //este constructor se construye al modelo de pedido                     
+            this.ReporteBase.LocalReport.ReportEmbeddedResource = "Reportes.Pedido.rdlc";
+            this.ReporteBase.LocalReport.Refresh();
+            this.ReporteBase.RefreshReport();
+            this.ReporteBase.LocalReport.DataSources.Clear();
+            this.ReporteBase.LocalReport.DataSources.Add(new ReportDataSource("EncabezadoPedido", p_ModeloPedido));
+            this.ReporteBase.LocalReport.DataSources.Add(new ReportDataSource("DetallePedido", p_ModeloPedido.lineasPedido));
+            this.ReporteBase.LocalReport.DataSources.Add(new ReportDataSource("ClientePedido", p_ModeloPersonas));
+            this.ReporteBase.LocalReport.DataSources.Add(new ReportDataSource("ClienteDomicilio", p_ModeloPersonas.domicilios));
+            this.ReporteBase.LocalReport.DataSources.Add(new ReportDataSource("ClienteMail", p_ModeloPersonas.mails));
+            this.ReporteBase.LocalReport.DataSources.Add(new ReportDataSource("ClienteTelefono", p_ModeloPersonas.telefonos));
+
             this.ReporteBase.RefreshReport();
         }
 
@@ -56,10 +73,10 @@ namespace Reportes
         private void SeteoParametrosGeneralesReporte()
         {
             ReportParameter[] parameters = new ReportParameter[4];
-            parameters[0] = new ReportParameter("parametroEmpresa", NombreEmpresa);
-            parameters[1] = new ReportParameter("parametroTelEmpresa", TelefonoEmpresa);
-            parameters[2] = new ReportParameter("parametroDirEmpresa", DireccionEmpresa);
-            parameters[3] = new ReportParameter("parametroFecVto", fechaVecimiendo);
+            parameters[0] = new ReportParameter("parametroEmpresa", glb_var_NombreEmpresa);
+            parameters[1] = new ReportParameter("parametroTelEmpresa", glb_var_TelefonoEmpresa);
+            parameters[2] = new ReportParameter("parametroDirEmpresa", glb_var_DireccionEmpresa);
+            parameters[3] = new ReportParameter("parametroFecVto", glb_var_fechaVecimiendo);
             this.ReporteBase.LocalReport.SetParameters(parameters);
         }
 
