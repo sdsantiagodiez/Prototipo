19d69dd38146096182031b1bfaa80c90801e029b
Santiago <sdsantiagodiez@gmail.com>
2016-05-09 18:16:33 -0300

    Se mejora la forma de obtener ticket afip

--- Controladores/ControladorAFIP.cs
+++ Controladores/ControladorAFIP.cs
@@ -10,122 +10,29 @@ namespace Controladores
 {
     public class ControladorAFIP : Controlador
     {
-        public static string facturarTEST()
+        string afipFolderPath;
+        string certificadoPath;
+        string passwordCertificado;
+        string cuitEmisor;
+        string ticketPath;
+
+        public ControladorAFIP()
         {
-            #region variables del certificado
-            string afipFolderPath = System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName + @"\AFIP";
-            string certificadoPath = afipFolderPath + @"\certificado.pfx";
-            string passwordCertificado = "prueba123";
-            string cuitEmisor = "20356445393"; //Debe ser el mismo cuit que el que está en el certificado
-            #endregion
-
-            string respuesta = "éxito";
-            WSAFIPFE.Factura fe = new WSAFIPFE.Factura();
-            Boolean bResultado = fe.iniciar(0, cuitEmisor, certificadoPath, ""); //0 modo homologación, 1 en producción. Sólo usar 0
-            if (bResultado)
-            {
-                fe.ArchivoCertificadoPassword = passwordCertificado;
-                bResultado = fe.f1ObtenerTicketAcceso();
-                if (bResultado)
-                {
-                    fe.F1CabeceraCantReg = 1;
-                    fe.F1CabeceraPtoVta = 1;
-                    fe.F1CabeceraCbteTipo = 1;
-
-                    fe.f1Indice = 0;
-                    fe.F1DetalleConcepto = 1;
-                    fe.F1DetalleDocTipo = 80;
-                    fe.F1DetalleDocNro = "20111111112";
-                    fe.F1DetalleCbteDesde = fe.F1CompUltimoAutorizado(fe.F1CabeceraPtoVta, fe.F1CabeceraCbteTipo) + 1;
-                    fe.F1DetalleCbteHasta = fe.F1CompUltimoAutorizado(fe.F1CabeceraPtoVta, fe.F1CabeceraCbteTipo) + 1;
-                    fe.F1DetalleCbteFch = DateTime.Today.ToString("yyyyMMdd");
-                    fe.F1DetalleImpTotal = 176.25;
-                    fe.F1DetalleImpTotalConc = 0;
-                    fe.F1DetalleImpNeto = 150;
-                    fe.F1DetalleImpOpEx = 0;
-                    //fe.F1DetalleImpTrib = 7.8;
-                    fe.F1DetalleImpIva = 26.25;
-                    //fe.F1DetalleFchServDesde = "20150315";
-                    //fe.F1DetalleFchVtoPago = "20150315";
-                    fe.F1DetalleMonId = "PES";//Moneda en PESOS ARGENTINOS
-                    fe.F1DetalleMonCotiz = 1;//1 debido a que es pesos
-
-                    //fe.F1DetalleTributoItemCantidad = 0;
-                    //fe.f1IndiceItem = 0;
-                    //fe.F1DetalleTributoId = 3;
-                    //fe.F1DetalleTributoDesc = "Impuesto Municipal Matanza";
-                    //fe.F1DetalleTributoBaseImp = 150;
-                    //fe.F1DetalleTributoAlic = 5.2;
-                    //fe.F1DetalleTributoImporte = 7.8;
-
-                    fe.F1DetalleIvaItemCantidad = 2;
-                    fe.f1IndiceItem = 0;
-                    fe.F1DetalleIvaId = 5;
-                    fe.F1DetalleIvaBaseImp = 100;
-                    fe.F1DetalleIvaImporte = 21;
-
-                    fe.f1IndiceItem = 1;
-                    fe.F1DetalleIvaId = 4;
-                    fe.F1DetalleIvaBaseImp = 50;
-                    fe.F1DetalleIvaImporte = 5.25;
-
-
-                    fe.F1DetalleCbtesAsocItemCantidad = 0;
-                    fe.F1DetalleOpcionalItemCantidad = 0;
-
-                    fe.ArchivoXMLRecibido = afipFolderPath + @"\XML\recibido.xml";
-                    fe.ArchivoXMLEnviado = afipFolderPath + @"\XML\enviado.xml";
-                    bResultado = fe.F1CAESolicitar();
-                    if (bResultado)
-                    {
-                        respuesta = "resultado verdadero ";
-                    }
-                    else
-                    {
-                        respuesta = "resultado falso ";
-                    }
-                    respuesta +=" | "+ "resultado global AFIP: " + fe.F1RespuestaResultado;
-                    respuesta +=" | "+ "es reproceso? " + fe.F1RespuestaReProceso;
-                    respuesta +=" | "+ "registros procesados por AFIP: " + fe.F1RespuestaCantidadReg.ToString();
-                    respuesta +=" | "+ "error genérico global:" + fe.f1ErrorMsg1;
-                    if (fe.F1RespuestaCantidadReg > 0)
-                    {
-                        fe.f1Indice = 0;
-                        respuesta +="|"+ "resultado detallado comprobante: " + fe.F1RespuestaDetalleResultado;
-                        respuesta +="|"+ "cae comprobante: " + fe.F1RespuestaDetalleCae;
-                        respuesta +="|"+ "número comprobante:" + fe.F1RespuestaDetalleCbteDesdeS;
-                        respuesta +="|"+ "error detallado comprobante: " + fe.F1RespuestaDetalleObservacionMsg1;
-                    }
-                }
-                else
-                {
-                    respuesta = "fallo acceso " + fe.UltimoMensajeError;
-                }
-            }
-            else
-            {
-                respuesta = "error inicio " + fe.UltimoMensajeError;
-            }
-            return respuesta;
+            afipFolderPath = System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName + @"\AFIP";
+            certificadoPath = afipFolderPath + @"\certificado.pfx";
+            passwordCertificado = "prueba123";
+            cuitEmisor = "20356445393"; //Debe ser el mismo cuit que el que está en el certificado
+            ticketPath = afipFolderPath + @"\ticket.txt";
         }
-
+    
         public bool facturar(ModeloPedido p_mod_pedido)
         {
-            #region variables del certificado
-            string afipFolderPath = System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName + @"\AFIP";
-            string certificadoPath = afipFolderPath + @"\certificado.pfx"; 
-            string passwordCertificado = "prueba123";
-            string cuitEmisor = "20356445393"; //Debe ser el mismo cuit que el que está en el certificado
-            #endregion 
-
             string respuesta ;
             WSAFIPFE.Factura fe = new WSAFIPFE.Factura();
             bool bResultado = fe.iniciar(0, cuitEmisor, certificadoPath, ""); //0 modo homologación, 1 en producción. Sólo usar 0
             if (bResultado)
-            {
-                fe.ArchivoCertificadoPassword = passwordCertificado;
-                bResultado = fe.f1ObtenerTicketAcceso();
-                if (bResultado)
+            {   
+                if (this.getTicketAcceso(fe))
                 {
                     ControladorAFIP.inicializarConstantes(fe);
                     ControladorAFIP.agregarDetallesPedido(fe, p_mod_pedido);
@@ -253,22 +160,16 @@ namespace Controladores
             p_mod_pedido.numeroComprobante = p_facturaElectronica.F1RespuestaDetalleCbteDesdeS;
         }
 
-        public static bool validarConexion()
+        /// <summary>
+        /// Válida conexión y ticket acceso para comunicarse con web services de AFIP
+        /// </summary>
+        /// <returns></returns>
+        public bool validarConexion()
         {
-            #region variables del certificado
-            string afipFolderPath = System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName + @"\AFIP";
-            string certificadoPath = afipFolderPath + @"\certificado.pfx";
-            string passwordCertificado = "prueba123";
-            string cuitEmisor = "20356445393"; //Debe ser el mismo cuit que el que está en el certificado
-            #endregion
-
             WSAFIPFE.Factura fe = new WSAFIPFE.Factura();
-            bool bResultado = fe.iniciar(0, cuitEmisor, certificadoPath, ""); //0 modo homologación, 1 en producción. Sólo usar 0
-            if (bResultado)
+            if (fe.iniciar(0, cuitEmisor, certificadoPath, "") )//0 modo homologación, 1 en producción. Sólo usar 0
             {
-                fe.ArchivoCertificadoPassword = passwordCertificado;
-                bResultado = fe.f1ObtenerTicketAcceso();
-                if (!bResultado)
+                if (!this.getTicketAcceso(fe))
                 {
                     return false;
                 }
@@ -278,6 +179,48 @@ namespace Controladores
                 return false;
             }
             return true;
+        }
+      
+        /// <summary>
+        /// Guarda ticket de acceso en archivo para posterior uso
+        /// </summary>
+        /// <param name="p_facturaElectronica"></param>
+        private void guardarTicketAcceso(WSAFIPFE.Factura p_facturaElectronica)
+        {
+            string ticket = p_facturaElectronica.f1GuardarTicketAcceso();
+            System.IO.File.WriteAllText(ticketPath,ticket);
+        }
+        /// <summary>
+        /// Retorna ticket de acceso guardado anteriormente
+        /// </summary>
+        /// <returns></returns>
+        private string readTicketAcceso()
+        {
+            if (System.IO.File.Exists(ticketPath))
+                return System.IO.File.ReadAllText(ticketPath);
+            else
+                return "";
+        }
+        /// <summary>
+        /// Obtiene ticket de acceso para conectarse a web service AFIP.
+        /// </summary>
+        /// <param name="p_facturaElectronica"></param>
+        /// <returns></returns>
+        private bool getTicketAcceso(WSAFIPFE.Factura p_facturaElectronica)
+        {
+            bool bResultado = false;
+            p_facturaElectronica.f1RestaurarTicketAcceso(this.readTicketAcceso());
+            if (p_facturaElectronica.f1TicketEsValido)
+            {
+                bResultado = true;
+            }
+            else
+            {
+                p_facturaElectronica.ArchivoCertificadoPassword = passwordCertificado;
+                bResultado = p_facturaElectronica.f1ObtenerTicketAcceso();
+                this.guardarTicketAcceso(p_facturaElectronica);
+            }
+            return bResultado;
         }
     }
 }
