42b9a492f3997b675e9a66cfef5230d00aee47f8
martinlj10 <martinlj10@hotmail.com>
2016-06-29 12:39:18 -0300

    Se agregan metodos de datos para los descuentos.

--- Datos/CatalogoDescuentos.cs
+++ Datos/CatalogoDescuentos.cs
@@ -27,7 +27,8 @@ namespace Datos
             return lcl_mod_descuento;
         }
 
-        public List<ModeloDescuento> buscarDescuento()
+
+        public List<ModeloDescuento> buscarDescuentos(string p_codigoOriginal, string p_codigoArticuloProveedor)
         {
             //Creo la conexion y la abro
             SqlConnection ConexionSQL = Conexion.crearConexion();
@@ -40,8 +41,12 @@ namespace Datos
 
             comando.CommandText =
                 "SELECT  [codigo_articulo_proveedor],[codigo_original],[fecha_desde],[fecha_hasta],[porcentaje_descuento],[numero_descuento]" +
-                "FROM  Descuentos_articulos" +
-                "WHERE ";
+                "FROM  Descuentos_articulos "+
+                "WHERE codigo_original like @codigo_original AND codigo_articulo_proveedor like @codigo_articulo_proveedor "+
+                "ORDER BY codigo_original desc";
+
+            comando.Parameters.Add(this.instanciarParametro(p_codigoOriginal, "@codigo_original"));
+            comando.Parameters.Add(this.instanciarParametro(p_codigoArticuloProveedor, "@codigo_articulo_proveedor"));
 
             comando.Connection.Open();
 
@@ -62,6 +67,132 @@ namespace Datos
             comando.Connection.Close();
 
             return lcl_lst_mod_descuento;
+        }
+
+        public bool descuentosSuperpuestos(string p_codigoOriginal, string p_codigoArticuloProveedor, DateTime p_fechaDesde, DateTime p_fechaHasta)
+        {
+            //Creo la conexion y la abro
+            SqlConnection ConexionSQL = Conexion.crearConexion();
+            //crea SQL command
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = ConexionSQL;
+            comando.CommandType = CommandType.Text;
+
+            //string querySQL = this.getCondicionBusqueda(p_mod_pedido, p_parametroBusqueda, ref comando);
+
+            comando.CommandText =
+                "SELECT  [codigo_articulo_proveedor],[codigo_original],[fecha_desde],[fecha_hasta],[porcentaje_descuento],[numero_descuento]" +
+                "FROM  Descuentos_articulos " +
+                "WHERE codigo_original like @codigo_original AND codigo_articulo_proveedor like @codigo_articulo_proveedor " +
+                "AND ((fecha_desde between @fecha_desde AND @fecha_hasta) OR (fecha_hasta between @fecha_desde AND @fecha_hasta))";
+                
+
+            comando.Parameters.Add(this.instanciarParametro(p_codigoOriginal, "@codigo_original"));
+            comando.Parameters.Add(this.instanciarParametro(p_codigoArticuloProveedor, "@codigo_articulo_proveedor"));
+            comando.Parameters.Add(this.instanciarParametro(p_fechaDesde, "@fecha_desde"));
+            comando.Parameters.Add(this.instanciarParametro(p_fechaHasta, "@fecha_hasta"));
+
+            comando.Connection.Open();
+
+            SqlDataReader drDescuentos = comando.ExecuteReader();
+            
+            bool rta = drDescuentos.HasRows;
+            drDescuentos.Close();
+            comando.Connection.Close();
+
+            return rta;
+        
+        }
+
+        public ModeloDescuento getOne (string p_numero_descuento)
+        {
+            //Creo la conexion y la abro
+            SqlConnection ConexionSQL = Conexion.crearConexion();
+            //crea SQL command
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = ConexionSQL;
+            comando.CommandType = CommandType.Text;
+
+            comando.CommandText =
+                "SELECT  [codigo_articulo_proveedor],[codigo_original],[fecha_desde],[fecha_hasta],[porcentaje_descuento],[numero_descuento]" +
+                "FROM  Descuentos_articulos "+
+                "WHERE numero_descuento like @numero_descuento "+
+                "ORDER BY codigo_original desc";
+
+            comando.Parameters.Add(this.instanciarParametro(p_numero_descuento, "@numero_descuento"));
+
+            comando.Connection.Open();
+
+            SqlDataReader drDescuentos = comando.ExecuteReader();
+
+            ModeloDescuento lcl_mod_descuento = new ModeloDescuento();
+
+            lcl_mod_descuento = this.leerDatosDescuento(drDescuentos);
+                      
+            drDescuentos.Close();
+            comando.Connection.Close();
+
+            return lcl_mod_descuento;
+        }
+        public int getUltimoNumero()
+        {
+            //Creo la conexion y la abro
+            SqlConnection ConexionSQL = Conexion.crearConexion();
+            //crea SQL command
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = ConexionSQL;
+            comando.CommandType = CommandType.Text;
+
+            comando.CommandText =
+                "SELECT  MAX [numero_descuento] as Ultimo" +
+                "FROM  Descuentos_articulos " ;
+
+                        comando.Connection.Open();
+
+            SqlDataReader drDescuentos = comando.ExecuteReader();
+
+            drDescuentos.Read();
+            int ultimo_descuento = (int)drDescuentos["Ultimo"];
+
+            drDescuentos.Close();
+            comando.Connection.Close();
+
+            return ultimo_descuento;
+        }
+
+        public bool add(ModeloDescuento p_mod_descuento)
+        {
+            if (this.descuentosSuperpuestos(p_mod_descuento.CodigoOriginal,p_mod_descuento.CodigoArticuloProveedor,p_mod_descuento.FechaDesde,p_mod_descuento.FechaHasta))
+            { throw new Exception("Existe otro descuento entre las fechas."); }
+
+            SqlConnection ConexionSQL = Conexion.crearConexion();
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = ConexionSQL;
+            comando.CommandType = CommandType.Text;
+            comando.CommandText =
+                "INSERT INTO [Descuentos_articulos]([codigo_original],[codigo_articulo_proveedor],[fecha_desde],[fecha_hasta]," +
+                "   [porcentaje_descuento],[numero_descuento]) " +
+                "   VALUES (@codigo_original, @codigo_articulo_proveedor, @fecha_desde, @fecha_hasta, @porcentaje_descuento, " +
+                "   @numero_descuento); ";
+                
+            comando.Parameters.Add(this.instanciarParametro(p_mod_descuento.CodigoArticuloProveedor, "@codigo_articulo_proveedor"));
+            comando.Parameters.Add(this.instanciarParametro(p_mod_descuento.CodigoOriginal, "@codigo_original"));
+            comando.Parameters.Add(this.instanciarParametro(p_mod_descuento.FechaDesde, "@fecha_desde"));
+            comando.Parameters.Add(this.instanciarParametro(p_mod_descuento.FechaHasta, "@fecha_hasta"));
+            comando.Parameters.Add(this.instanciarParametro(p_mod_descuento.numeroDescuento, "@numero_descuento"));
+            comando.Parameters.Add(this.instanciarParametro(p_mod_descuento.PorcentajeDescuento, "@porcentaje_descuento"));
+                       
+
+            comando.Connection.Open();
+            int rowaffected = comando.ExecuteNonQuery();
+            comando.Connection.Close();
+            
+            if (rowaffected < 1)
+            {
+                throw new Exception("Ha ocurrido un error en la base de datos. No se ha podido registrar Descuento.");
+            }
+
+            return true;
         }
 
 
