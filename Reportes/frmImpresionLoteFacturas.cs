bb1df88e89eeb8a6da11338a3504ca94d97499d8
martinlj10 <martinlj10@hotmail.com>
2016-07-16 12:48:11 -0300

    intento de reimpresion de varios comprobantes.

--- Reportes/frmImpresionLoteFacturas.cs
+++ Reportes/frmImpresionLoteFacturas.cs
@@ -8,7 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Modelos;
+using System.Drawing.Printing;
+using Microsoft.Reporting;
 using Microsoft.Reporting.WinForms;
+using System.IO;
 
 
 namespace Reportes
@@ -23,48 +26,62 @@ namespace Reportes
         public frmImpresionLoteFacturas(List<ModeloReporteEncabezadoFactura> p_lst_mod_EncFac, string p_tipoComprobante)
         {
             InitializeComponent();
-
-            ModeloReporteEncabezadoFacturaBindingSource.Clear();
-            modeloReporteDetalleFacturaBindingSource.Clear();
-            modeloReporteDetalleFacturaBindingSource.DataSource = typeof(List<ModeloReporteDetalleFactura>);
-            ModeloReporteEncabezadoFacturaBindingSource.DataSource = p_lst_mod_EncFac;
-            modeloReporteDetalleFacturaBindingSource.DataSource = p_lst_mod_EncFac;
-           /* foreach (ModeloReporteEncabezadoFactura mod in p_lst_mod_EncFac)
+            foreach (ModeloReporteEncabezadoFactura mod in p_lst_mod_EncFac)
             {
+                Warning[] warnings;
+                string[] streamids;
+                string mimeType;
+                string encoding;
+                string extension;
+
+                ModeloReporteEncabezadoFacturaBindingSource.Clear();
+                modeloReporteDetalleFacturaBindingSource.Clear();
+                modeloReporteDetalleFacturaBindingSource.DataSource = typeof(List<ModeloReporteDetalleFactura>);
+                //ModeloReporteEncabezadoFacturaBindingSource.DataSource = p_lst_mod_EncFac;
+                //modeloReporteDetalleFacturaBindingSource.DataSource = p_lst_mod_EncFac;
+
                 //ModeloReporteEncabezadoFacturaBindingSource.DataSource = mod;
                 //modeloReporteDetalleFacturaBindingSource.DataSource = mod.detalleFactura;
-                modeloReporteDetalleFacturaBindingSource.Add(mod);
-                modeloReporteDetalleFacturaBindingSource.Add(mod.detalleFactura);
-            }*/
-            
-            
-            this.contenedorLote.LocalReport.DataSources.Clear();
-            this.contenedorLote.LocalReport.DataSources.Add(new ReportDataSource("DSFactura", ModeloReporteEncabezadoFacturaBindingSource));
-            this.contenedorLote.LocalReport.DataSources.Add(new ReportDataSource("DSDetalleFactura", modeloReporteDetalleFacturaBindingSource));
+                modeloReporteDetalleFacturaBindingSource.DataSource=mod;
+                modeloReporteDetalleFacturaBindingSource.DataSource=mod.detalleFactura;
 
-            if (String.Equals(p_tipoComprobante, "1"))
-            {
-                this.contenedorLote.LocalReport.ReportEmbeddedResource = "Reportes.FacturaA.rdlc";
-            }
-            else if (String.Equals(p_tipoComprobante, "6"))
-            {
-                this.contenedorLote.LocalReport.ReportEmbeddedResource = "Reportes.FacturaB.rdlc";
-            }
-            else if (String.Equals(p_tipoComprobante, "3"))
-            {
-                this.contenedorLote.LocalReport.ReportEmbeddedResource = "Reportes.NCreditoA.rdlc";
-            }
-            else if (String.Equals(p_tipoComprobante, "8"))
-            {
-                this.contenedorLote.LocalReport.ReportEmbeddedResource = "Reportes.NCreditoB.rdlc";
-            }
-            else if (String.Equals(p_tipoComprobante, "0"))
-            {
-                this.contenedorLote.LocalReport.ReportEmbeddedResource = "Reportes.Pedido.rdlc";
-            }
-            this.contenedorLote.LocalReport.Refresh();
-            this.contenedorLote.RefreshReport();
 
+
+                this.contenedorLote.LocalReport.DataSources.Clear();
+                this.contenedorLote.LocalReport.DataSources.Add(new ReportDataSource("DSFactura", ModeloReporteEncabezadoFacturaBindingSource));
+                this.contenedorLote.LocalReport.DataSources.Add(new ReportDataSource("DSDetalleFactura", modeloReporteDetalleFacturaBindingSource));
+
+                if (String.Equals(p_tipoComprobante, "1"))
+                {
+                    this.contenedorLote.LocalReport.ReportEmbeddedResource = "Reportes.FacturaA.rdlc";
+                }
+                else if (String.Equals(p_tipoComprobante, "6"))
+                {
+                    this.contenedorLote.LocalReport.ReportEmbeddedResource = "Reportes.FacturaB.rdlc";
+                }
+                else if (String.Equals(p_tipoComprobante, "3"))
+                {
+                    this.contenedorLote.LocalReport.ReportEmbeddedResource = "Reportes.NCreditoA.rdlc";
+                }
+                else if (String.Equals(p_tipoComprobante, "8"))
+                {
+                    this.contenedorLote.LocalReport.ReportEmbeddedResource = "Reportes.NCreditoB.rdlc";
+                }
+                else if (String.Equals(p_tipoComprobante, "0"))
+                {
+                    this.contenedorLote.LocalReport.ReportEmbeddedResource = "Reportes.Pedido.rdlc";
+                }
+                
+                this.contenedorLote.LocalReport.Refresh();
+                this.contenedorLote.RefreshReport();
+                byte[] bytes = contenedorLote.LocalReport.Render("PDF", null, out mimeType, out encoding, out extension, out streamids, out warnings);
+                
+                FileStream fs = new FileStream(@"c:\users\lopejmar\desktop\Pedido "+ mod.Remito +".pdf", FileMode.Create);
+                fs.Write(bytes, 0, bytes.Length);
+                fs.Close();
+
+                
+            }
         }
 
         private void frmImpresionLoteFacturas_Load(object sender, EventArgs e)
