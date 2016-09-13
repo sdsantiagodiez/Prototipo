425de14740a1a54fffc21aa6e2a2e0842b6281bf
sdsantiagodiez <sdsantiagodiez@gmail.com>
2016-09-13 20:54:10 -0300

    Se agrega mail y telefono para algunos reportes

--- Modelos/ModeloReporteEncabezadoComprobante.cs
+++ Modelos/ModeloReporteEncabezadoComprobante.cs
@@ -12,6 +12,8 @@ namespace Modelos
         public List<ModeloReporteDetalleComprobante> detalleFactura {get;set;}
         public string Comprador_RazonSocial { get; set; }
         public string Comprador_Domicilio { get; set; }
+        public string Comprador_Mail { get; set; }
+        public string Comprador_Telefono { get; set; }
         public string Comprador_Cuit { get; set; }
         public string Comprador_IVAResponsableI { get; set; }
         public string Remito { get; set; }
@@ -35,35 +37,58 @@ namespace Modelos
 
         public ModeloReporteEncabezadoComprobante(ModeloPedido p_mod_pedido) 
         {
-            // Carga en modelo Reportepedido Que hacemos si no esta aprobado, donde validamos?
-            // Si no esta aprobado por afip, imprimimos igual
-            
-            this.detalleFactura = new List<ModeloReporteDetalleComprobante>();
+            this.setDatosBasicosComprobante(p_mod_pedido);
+            this.setDatosReceptorComprobante(p_mod_pedido);
+            this.setDetalleFactura(p_mod_pedido);
+            this.setMontos(p_mod_pedido);
+        }
+
+        private void setDatosBasicosComprobante(ModeloPedido p_mod_pedido)
+        {
+            this.Alicuota = Convert.ToDecimal(p_mod_pedido.alicuota.iva.porcentaje);
             this.CAINumero = p_mod_pedido.CAE;
             //this.CodigodeBarras = LibreriaClasesCompartidas.Transformar.imagenCodigoBarras(p_mod_pedido.getCodigoBarraCAE());
             string codigoBarrasCAE = p_mod_pedido.getCodigoBarraCAE();
             this.CodigodeBarras = codigoBarrasCAE == null ? codigoBarrasCAE : LibreriaClasesCompartidas.Transformar.codigoBarras(codigoBarrasCAE.ToCharArray());
             //this.CodigodeBarras = LibreriaClasesCompartidas.Transformar.codigoBarras(p_mod_pedido.getCodigoBarraCAE().ToCharArray());//Codigo de barra luego del algoritmo
             //this.CodigodeBarrasImagen = LibreriaClasesCompartidas.Interleaved2of5.gifInterleaved(p_mod_pedido.getCodigoBarraCAE());
-            this.CodigodeBarrasImagen = this.CodigodeBarras== null? this.CodigodeBarras: LibreriaClasesCompartidas.Transformar.imagenCodigoBarras(this.CodigodeBarras); 
-            this.Alicuota = Convert.ToDecimal(p_mod_pedido.alicuota.iva.porcentaje);
+            this.CodigodeBarrasImagen = this.CodigodeBarras == null ? this.CodigodeBarras : LibreriaClasesCompartidas.Transformar.imagenCodigoBarras(this.CodigodeBarras);
             this.CuitEmisor = ModeloPedido.cuitEmisor;
             this.CentroEmisor = "0001";//p_mod_pedido.numeroComprobanteAFIP;
-            this.NumeroComprobante = (p_mod_pedido.numeroComprobanteAFIP!=null)?p_mod_pedido.numeroComprobanteAFIP.PadLeft(8, '0'):null;
+            this.NumeroComprobante = (p_mod_pedido.numeroComprobanteAFIP != null) ? p_mod_pedido.numeroComprobanteAFIP.PadLeft(8, '0') : null;
             this.NumeroControl = p_mod_pedido.numeroPedido;
-            this.Comprador_Cuit = (p_mod_pedido.entidad.cuit != null )? p_mod_pedido.entidad.cuit:p_mod_pedido.documentoComprador.numero;
-            this.Comprador_IVAResponsableI = this.defineSituacionIVA((int)p_mod_pedido.entidad.codigoTipoResponsable);
-            if (p_mod_pedido.domicilioDeFacturacion == null)
-            { this.Comprador_Domicilio = ""; }
-            else
-            {
-                this.Comprador_Domicilio = p_mod_pedido.domicilioDeFacturacion.ToString();
-            }
-            this.Comprador_RazonSocial = p_mod_pedido.entidad.ToString();
             //lcl_mod_Factura.ConceptosNoGravados = p_mod_pedido.
             this.CondicionVenta = (p_mod_pedido.formasDePago != null) ? p_mod_pedido.formasDePago[0].forma.ToString() : null;
-                       
-            
+            this.FechaComprobante = p_mod_pedido.fecha;
+            this.FechaVencimiento = p_mod_pedido.VencimientoCAE;
+            //this.Remito = p_mod_pedido.numeroPedido.ToString().PadLeft(8,'0');
+            this.Remito = (p_mod_pedido.numeroComprobante > 0) ? p_mod_pedido.numeroComprobante.ToString().PadLeft(8, '0') : null;
+            this.CopiaComprobante = p_mod_pedido.copiaComprobante;
+        }
+
+        /// <summary>
+        /// Receptor comprobante ya sea Cliente o Proveedor
+        /// </summary>
+        /// <param name="p_mod_pedido"></param>
+        private void setDatosReceptorComprobante(ModeloPedido p_mod_pedido)
+        {
+            //por que se hace esta validación con el cuit de la entidad si ya tenemos el documentoComprador en el pedido?
+            this.Comprador_Cuit = (p_mod_pedido.entidad.cuit != null) ? p_mod_pedido.entidad.cuit : p_mod_pedido.documentoComprador.numero;
+            this.Comprador_IVAResponsableI = GetSituacionIVA((int)p_mod_pedido.entidad.codigoTipoResponsable);
+            this.Comprador_Domicilio = p_mod_pedido.domicilioDeFacturacion == null ? "Sin Domicilio" : p_mod_pedido.domicilioDeFacturacion.ToString();
+            this.Comprador_Mail = p_mod_pedido.mailContacto == null?"Sin Mail":p_mod_pedido.mailContacto.mail;
+            this.Comprador_Telefono = p_mod_pedido.telefonoContacto == null? "Sin Teléfono":p_mod_pedido.telefonoContacto.numero;
+
+
+            this.Comprador_RazonSocial = p_mod_pedido.entidad.ToString();
+        }
+        /// <summary>
+        /// Carga artículos y descuentos del pedido
+        /// </summary>
+        /// <param name="p_mod_pedido"></param>
+        private void setDetalleFactura(ModeloPedido p_mod_pedido)
+        {
+            this.detalleFactura = new List<ModeloReporteDetalleComprobante>();
             foreach (ModeloLineaPedido lp in p_mod_pedido.lineasPedido)
             {
                 this.detalleFactura.Add(new ModeloReporteDetalleComprobante(lp));
@@ -75,46 +100,45 @@ namespace Modelos
                     {
                         motivos = motivos + " - " + d.descripcion;
                         monto = monto + d.montoDescontadoSobreTotal;
-                       // this.detalleFactura.Add(new ModeloReporteDetalleComprobante(lp.articulo,d));
+                        // this.detalleFactura.Add(new ModeloReporteDetalleComprobante(lp.articulo,d));
                     }
-                    this.detalleFactura.Add(new ModeloReporteDetalleComprobante(new ModeloLineaPedido(monto,motivos,lp.articulo.codigoArticuloProveedor)));
+                    this.detalleFactura.Add(new ModeloReporteDetalleComprobante(new ModeloLineaPedido(monto, motivos, lp.articulo.codigoArticuloProveedor)));
                 }
             }
             if (p_mod_pedido.descuentos.descuento_total_monto > 0)
             {
                 this.detalleFactura.Add((this.agregaLineaDescuentoGlobal(p_mod_pedido.descuentos.descuento_total_monto)));
             }
-
-
-            this.FechaComprobante = p_mod_pedido.fecha;
-            this.FechaVencimiento = p_mod_pedido.VencimientoCAE;
+        }
+        /// <summary>
+        /// Datos monetarios (iva, subtotales, totales)
+        /// </summary>
+        /// <param name="p_mod_pedido"></param>
+        private void setMontos(ModeloPedido p_mod_pedido)
+        {
             this.IVAComprobante = p_mod_pedido.alicuota.monto;
-            //this.Remito = p_mod_pedido.numeroPedido.ToString().PadLeft(8,'0');
-            this.Remito = (p_mod_pedido.numeroComprobante > 0)? p_mod_pedido.numeroComprobante.ToString().PadLeft(8, '0'):null;
             this.SubtotalComprobante = p_mod_pedido.montoSubTotal;
             this.TotalComprobante = p_mod_pedido.montoTotal;
             this.TotalComprobanteLetras = LibreriaClasesCompartidas.Transformar.NumeroALetras(p_mod_pedido.montoTotal.ToString());
-            this.CopiaComprobante = p_mod_pedido.copiaComprobante;
         }
 
-        public string defineSituacionIVA(int p_situacionIVA)
-        { string situacion ="";
-        switch (p_situacionIVA)
-        {
-            case 5: situacion = "Consumidor Final"; break;
-            case 6: situacion = "Monotributista"; break;
-            case 1: situacion = "Responsable Inscripto"; break;
-            default: situacion = "Consumidor Final"; break;
-        }
-
-        return situacion;
-        }
         public ModeloReporteDetalleComprobante agregaLineaDescuentoGlobal(decimal p_montoDescuento)
         {
-           ModeloReporteDetalleComprobante lcl_mod_detalle_descuento = new ModeloReporteDetalleComprobante(new ModeloLineaPedido(p_montoDescuento));
-
-            return lcl_mod_detalle_descuento;
+            return new ModeloReporteDetalleComprobante(new ModeloLineaPedido(p_montoDescuento));
         }
       
+        public static string GetSituacionIVA(int p_situacionIVA)
+        {
+            string situacion = "";
+            switch (p_situacionIVA)
+            {
+                case 5: situacion = "Consumidor Final"; break;
+                case 6: situacion = "Monotributista"; break;
+                case 1: situacion = "Responsable Inscripto"; break;
+                default: situacion = "Consumidor Final"; break;
+            }
+
+            return situacion;
+        }
     }
 }
